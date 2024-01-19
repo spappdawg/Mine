657e68a15389b6fd06051d573880af96bbf406e9
spappdawg <spappy.miller@gmail.com>
2024-01-18 20:20:41 -0800

    Create Intro.py

---
+++ Intro.py
@@ -0,0 +1,2 @@
+statement = "I haven't written code for business in at least 5 years. I am a leader with 15+ years experience leading the best in the businees at writing code and securing networks."
+print(statement)
